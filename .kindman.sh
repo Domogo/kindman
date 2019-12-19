3e071b529af0faa71cf47ac7bfe79a4752cdcc39
Domogo <domsatrapa@gmail.com>
2019-12-19 19:08:26 +0100

    create a dedicated dir in kindle device

--- .kindman.sh
+++ .kindman.sh
@@ -7,10 +7,20 @@ function kindman() {
     case $opt in
         d) kindle_directory="$OPTARG"
         ;;
-        \?) echo "Invalid argument -$OPTARG" >&2
+        \?) echo "Invalid argument -$OPTARG" >&2 && return 1
         ;;
     esac
     done
 
-    printf "Argument arg_1 is %s\n" "$kindle_directory"
+    # create a dir in which to store the books
+    kindle_location=/run/media/domagoj/Kindle/documents/
+    mkdir $kindle_location/$kindle_directory
+
+    # skips first two arguments ( -d flag and it's assigned value )
+    # unnamed args are book names - go through each one and
+    # put it in kindle
+    for var in "${@:3}"
+    do
+        echo "$var"
+    done
 }
