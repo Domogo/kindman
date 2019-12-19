3c8e771b05fb1681cce29d9e84c3b69d1d0bc218
Domogo <domsatrapa@gmail.com>
2019-12-19 19:50:40 +0100

    -m

--- .kindman.sh
+++ .kindman.sh
@@ -3,9 +3,11 @@
 # Kindman - kindle manager
 
 function kindman() {
-    while getopts ":d:" opt; do
+    while getopts ":k:l:" opt; do
     case $opt in
-        d) kindle_directory="$OPTARG"
+        k) kindle_directory="$OPTARG"
+        ;;
+        l) local_directory="$OPTARG"
         ;;
         \?) echo "Invalid argument -$OPTARG" >&2 && return 1
         ;;
@@ -13,14 +15,16 @@ function kindman() {
     done
 
     # create a dir in which to store the books
-    kindle_location=/run/media/domagoj/Kindle/documents/
-    mkdir $kindle_location/$kindle_directory
+    # -p flag - don't create dir if already exists
+    kindle_location=/run/media/domagoj/Kindle/documents
+    book_location=$HOME/documents/books/$local_directory
+    mkdir -p $kindle_location/$kindle_directory
 
     # skips first two arguments ( -d flag and it's assigned value )
     # unnamed args are book names - go through each one and
     # put it in kindle
-    for var in "${@:3}"
+    for var in "${@:5}"
     do
-        echo "$var"
+        cp -i $book_location/$var $kindle_location/$kindle_directory
     done
 }
